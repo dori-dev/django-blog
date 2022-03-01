e50dc0dac2eaa3891b12046b4607c391a0f89016
mohammad <mr.dori.dev@gmail.com>
2022-03-01 09:40:31 +0330

    add detail of articles

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -4,18 +4,17 @@ from . import models
 
 def articles_list(request):
     """articles list"""
-    articles = models.Article.objects.all().order_by("date")[::-1]
+    articles = list(reversed(models.Article.objects.all().order_by("date")))[:20]
     args = {
         "articles": articles
     }
     return render(request, "articles/articles.html", args)
 
 
-def article_detail(request, slug):  # TODO
+def article_detail(request, slug):
     """article detail"""
     article = models.Article.objects.get(slug=slug)
     args = {
         "article": article
     }
     return render(request, "articles/article-detail.html", args)
-    
