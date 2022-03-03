b7d6213721c5027339a182af0033b7bca312bf9c
mohammad <mr.dori.dev@gmail.com>
2022-03-03 21:19:10 +0330

    fix csrf token error

--- blog/accounts/urls.py
+++ blog/accounts/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("signup", views.signup_view, name="signup"),
-    path("login", views.login_view, name="login"),
+    # path("login", views.login_view, name="login"),
 ]
--- blog/accounts/views.py
+++ blog/accounts/views.py
@@ -1,20 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 
 def signup_view(request):
     """signup view"""
-    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # login
+            return redirect("articles:list")
+        # form = UserCreationForm()
+    else:
+        form = UserCreationForm()
     arg = {
         "form": form,
     }
     return render(request, "accounts/signup.html", arg)
 
 
-def login_view(request):
-    """signup view"""
-    form = UserCreationForm()
-    arg = {
-        "form": form,
-    }
-    return render(request, "accounts/login.html", arg)
+# def login_view(request):
+#     """signup view"""
+#     form = UserCreationForm()
+#     arg = {
+#         "form": form,
+#     }
+#     return render(request, "accounts/login.html", arg)
