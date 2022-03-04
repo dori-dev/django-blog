2511c9f9fcae5c15c014184707d985dbd7af8647
mohammad <mr.dori.dev@gmail.com>
2022-03-04 11:24:44 +0330

    use dynamic url

--- blog/accounts/urls.py
+++ blog/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = "accounts"
 urlpatterns = [
     path("register", views.register_view, name="register"),
     path("login", views.login_view, name="login"),
--- blog/accounts/views.py
+++ blog/accounts/views.py
@@ -10,8 +10,8 @@ def register_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            # login
+            user = form.save()
+            login(request, user)
             return redirect("articles:list")
     form = UserCreationForm()
     arg: dict = {
@@ -26,7 +26,6 @@ def login_view(request):
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            # login use
             user = form.get_user()
             login(request, user)
             return redirect("articles:list")
