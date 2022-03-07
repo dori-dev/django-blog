9ad28d9c8c3c21d70f141c7e291b7e946eded77b
mohammad <mr.dori.dev@gmail.com>
2022-03-07 15:16:04 +0330

    change fa to fa-IR

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'fa'
+LANGUAGE_CODE = 'fa-IR'
 
 TIME_ZONE = 'Asia/Tehran'
 
