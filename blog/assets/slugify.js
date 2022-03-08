80a023f07318270b3feb9caae2d9918d3526fdd0
mohammad <mr.dori.dev@gmail.com>
2022-03-08 16:12:58 +0330

    support persian in auto slugify

--- blog/assets/slugify.js
+++ blog/assets/slugify.js
@@ -5,7 +5,14 @@ const slugify = (val) => {
   return val
     .toString()
     .trim()
-    .replace(/[\s\W-]+/g, "-");
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w\u0621-\u064A0-9-گدٔچپژک ٓی]+/g, "")
+    .replace(/\-\-+/g, "-")
+    .replace(/^-+/g, "")
+    .replace(/-+$/g, "")
+    .replace(/x/g, "")
+    .replace(/x/g, "");
 };
 
 titleInput.addEventListener("keyup", (e) => {
